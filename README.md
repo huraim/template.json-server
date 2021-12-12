d9fafe378615112e0cf23578da4ef245da6d0f3a
taka <taka@mewpot.com>
2021-12-12 19:36:57 +0900

    [Default] 가벼운 개발 테스트를 위한 샘플 서버 생성

---
+++ README.md
@@ -0,0 +1,23 @@
+React hooks 공부할 때 이용한 샘플 서버입니다.
+
+노션 출처
+https://www.notion.so/7b61ace07bd94744a7cedc1bec1d1bf2
+
+# 1. 서버를 구동시킬 폴더를 만듭니다.
+mkdir todo-server
+cd todo-server
+
+# 2. 초기화 및 설치
+npm init
+// 위 과정 이후 엔터 7~8번 진행
+npm install express --save
+npm install cors
+
+# 3. 서버 파일 만들기
+현재 레파지토리의 app.js를 만든것
+
+# 4. 해당 폴더 내 터미널 실행
+node index.js
+
+# 5. 서버 확인
+http://localhost:8080/todos
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 8080;
+
+const todos = [
+  {
+    id: 1,
+    title: "React Hooks 익히기",
+    status: "todo"
+  },
+  {
+    id: 2,
+    title: "GraphQL 익히기",
+    status: "todo"
+  },
+  {
+    id: 3,
+    title: "Apollo 익히기",
+    status: "todo"
+  }
+];
+
+app.use(cors());
+app.options("*", cors());
+
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/todos", (req, res) => res.json(todos));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
